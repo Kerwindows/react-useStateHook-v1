cf3879b75c6e775addf698b5431fdaa46ce0867f
kerwindows <kerwindows@hotmail.com>
2022-09-30 07:27:52 -0400

    added useState Hooks

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # react-useStateHook-v1
 
+This app show how to use a button click and setInterval to change `useState` hook
+
 [View Live:(https://kerwindows.github.io/react-useStateHook-v1)]
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
+
+
 
 function App() {
+  const time = new Date().toLocaleTimeString();
+
+  const [isTime,setTime] = useState(time);
+  const [isClock,setClock] = useState(time);
+
+const getTime = () => {
+  const newTime = new Date().toLocaleTimeString();
+  setTime(newTime);
+}
+
+const getClock = () => {
+  const newClock = new Date().toLocaleTimeString();
+  setClock(newClock);
+}
+setInterval(getClock, 1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <p>Clock</p>
+      <h2>{isClock}</h2>
+      <p>Button Click</p>
+      <h1>{isTime}</h1>
+      
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
